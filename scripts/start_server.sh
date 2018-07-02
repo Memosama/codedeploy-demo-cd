83ac5c5434262b416f13c6a6c9338c29087ff6a0
alemazz <am@ideato.it>
2018-07-02 18:09:14 +0200

    test a lemp app

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-sudo yum -y wget epel-release
+sudo yum -y install yum-utils wget epel-release
 wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 wget http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 rpm -Uvh remi-release-7.rpm epel-release-latest-7.noarch.rpm
-yum install yum-utils
 yum-config-manager --enable remi-php72
 sudo yum -y install nginx php-fpm
 mkdir -p /tmp/wordpress-cache
